21a3e309c1d2ddc88122e8029d2b88a3f486d42e
= <noivanmondfrans@gmail.com>
2023-02-07 11:38:18 +0100

    Try and move to a monorepo approach

---
+++ packages/fetch/lib/index.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * This is a place-holder library that has not yet been published
+ *
+ * @module @Prelude-IO/Fetch
+ */
+
+/**
+ * This is a place-holder library that has not yet been published
+ */
+export default function ioFetch() {
+  return "This is a place-holder library that has not yet been published";
+}
---
+++ packages/fetch/tests/fetch.spec.ts
@@ -0,0 +1,8 @@
+import { describe } from "mocha";
+import assert from "assert";
+
+describe("ioFetch", () => {
+  it("works as a sub-package", () => {
+    assert.ok(true);
+  });
+});
